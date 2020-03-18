415a3fb2b6a1d9a65c3530dc60372bb87021f134
Edge <Androedge@gmail.com>
2020-03-19 02:25:39 +0300

    DZ complete

--- Basics/home_work/js/script.js
+++ Basics/home_work/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 //Восстановить порядок в меню, добавить пятый пункт
 function stepOne() {
 
@@ -28,4 +29,50 @@ function stepOne() {
     arrayMenuItems.forEach(val => classMenu.appendChild(val));
 }
 
+//Заменить картинку заднего фона на другую из папки img
+function stepTwo() {
+    let bodyEl = document.querySelector('body');
+    bodyEl.style.backgroundImage = "url('img/apple_true.jpg')";
+}
+
+//Поменять заголовок, добавить слово "подлинную" ( Получится - "Мы продаем только подлинную технику Apple")
+function stepThree() {
+    let correctedString = '';
+
+    let divClassTitleEl = document.querySelector('.title');
+    let textNode = divClassTitleEl.innerHTML;
+    let search = textNode.indexOf('технику');
+
+    correctedString = textNode.substring(0, search).concat('подлинную ')
+        .concat(textNode.substring(search, textNode.length - 1));
+    divClassTitleEl.innerHTML = correctedString;
+}
+
+//Удалить рекламу со страницы
+function stepFour() {
+    document.querySelector('.adv').remove();
+}
+
+//Спросить у пользователя отношение к технике apple и записать ответ в блок на странице с id "prompt"
+function stepFive() {
+    setTimeout(userInteract, 500);
+}
+
+function userInteract() {
+    let question = prompt('Как Вы относитесь к технике Apple?', '');
+    let answer = question != null ? question : '';
+    const header = 'Ваше отношение к технике Apple:';
+    let classPromptEl = document.querySelector('.prompt');
+    let paragraphEl = document.createElement('p');
+
+    paragraphEl.appendChild(document.createTextNode(answer));
+    classPromptEl.appendChild(document.createTextNode(header));
+    classPromptEl.appendChild(paragraphEl); 
+}
+
+
 stepOne();
+stepTwo();
+stepThree();
+stepFour();
+stepFive();
